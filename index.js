f175fcbfadcb1ba76eb9b48c26fde81a48e50e0c
Lucas <luqui.llano.04@gmail.com>
2022-06-20 08:48:39 -0300

    .

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
   .then((res) => res.json())
   .then((data) => console.log(data["0"]["casa"]["nombre"]));
 
-function fetchear(dol) {
-  fetch("api.json")
-    .then((res) => res.json())
-    .then((data) => document.write(data[dol]["casa"]));
-}
-
-fetchear(0);
+// function fetchear(dol) {
+//   fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+//     .then((res) => res.json())
+//     .then((data) => document.write(data[dol]["casa"]));
+// }
