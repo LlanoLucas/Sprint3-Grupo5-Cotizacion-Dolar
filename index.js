73a73f4d09caf04b4aec52b9007c7bef11a26cb5
Lucas <luqui.llano.04@gmail.com>
2022-06-20 17:35:18 -0300

    todavía me faltan cosas, pero es por ahí bro

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let contenido = document.querySelector(
-  ".col-4 card bg-secondary bg-opacity-25 me-3"
-);
+let contenido = document.querySelector("#dolares");
 
 function fetchear() {
   fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
@@ -9,46 +7,55 @@ function fetchear() {
       pintar(data);
     });
 }
-console.log(contenido);
 
-// function pintar(data) {
-//   contenido.innerHTML += `
+fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
 
-//   <div class="row">
-//             <div class="col-12 p-2 bg-success bg-opacity-50">
-//               <p class="m-0 text-center text-white">DOLAR OFICIAL</p>
-//             </div>
-//             <div class="col-12 py-4 d-flex flex-row justify-content-around">
-//               <div class="d-flex flex-column">
-//                 <p class="m-0 text-bold title-buy-sell text-center">Compra</p>
-//                 <p
-//                   class="m-0 text-bold currency-font text-opacity-50 text-danger"
-//                 >
-//                   $205,04
-//                 </p>
-//               </div>
-//               <div class="d-flex flex-column">
-//                 <p class="m-0 title-buy-sell text-center">Venta</p>
-//                 <p class="m-0 currency-font text-opacity-50 text-secondary">
-//                   $200,10
-//                 </p>
-//               </div>
-//             </div>
-//             <div
-//               class="col-12 pt-2 text-center p-1 bg-light bg-gradient bg-opacity-50"
-//             >
-//               <p class="m-0 text-danger">Variacion +0.2%</p>
-//             </div>
-//             <div
-//               class="col-12 pb-2 text-center bg-light bg-gradient bg-opacity-50"
-//             >
-//               <p class="m-0 date-font text-muted">
-//                 ACTUALIZADO 19/06/2022 18:00
-//               </p>
-//             </div>
-//           </div>
+function pintar(data) {
+  for (let valores of data) {
+    contenido.innerHTML += `
 
-//   `;
-// }
+  <div class="row">
+            <div class="col-12 p-2 bg-success bg-opacity-50">
+              <p class="m-0 text-center text-white">${
+                data[valores["nombre"]]
+              }</p>
+            </div>
+            <div class="col-12 py-4 d-flex flex-row justify-content-around">
+              <div class="d-flex flex-column">
+                <p class="m-0 text-bold title-buy-sell text-center">Compra</p>
+                <p
+                  class="m-0 text-bold currency-font text-opacity-50 text-danger"
+                >
+                  ${data["0"]["casa"]["compra"]}
+                </p>
+              </div>
+              <div class="d-flex flex-column">
+                <p class="m-0 title-buy-sell text-center">Venta</p>
+                <p class="m-0 currency-font text-opacity-50 text-secondary">
+                  ${data["0"]["casa"]["venta"]}
+                </p>
+              </div>
+            </div>
+            <div
+              class="col-12 pt-2 text-center p-1 bg-light bg-gradient bg-opacity-50"
+            >
+              <p class="m-0 text-danger">${data["0"]["casa"]["variacion"]}%</p>
+            </div>
+            <div
+              class="col-12 pb-2 text-center bg-light bg-gradient bg-opacity-50"
+            >
+              <p class="m-0 date-font text-muted">
+                ACTUALIZADO 19/06/2022 18:00
+              </p>
+            </div>
+          </div>
 
-// fetchear();
+  `;
+  }
+}
+
+fetchear();
